70bd48002a9c4379a4b172e676e398d4c598e618
Alexander Malandin <115172406+Slend37@users.noreply.github.com>
2024-10-01 15:03:23 +0300

    Update doubleChar.java

--- hw java 4/doubleChar.java
+++ hw java 4/doubleChar.java
@@ -1,7 +1,7 @@
 public String doubleChar(String str) {
   String b = new String();
   for (int i = 0; i < str.length(); i++){
-    b = b + str.substring(i, i + 1)+str.substring(i, i + 1);
+    b = b + str.substring(i, i + 1) + str.substring(i, i + 1);
   }
   return b;
 }
