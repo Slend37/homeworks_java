620008a75455aac083fe0b42887524f0982dbe82
Alexander Malandin <115172406+Slend37@users.noreply.github.com>
2024-10-01 15:02:13 +0300

    Update catDog.java

--- hw java 4/catDog.java
+++ hw java 4/catDog.java
@@ -2,10 +2,10 @@ public boolean catDog(String str) {
   int k_cat = 0;
   int k_dog = 0;
   
-  for (int i = 0; i<str.length()-2; i++){
-    if (str.substring(i,i+3).equals("cat")){
+  for (int i = 0; i < str.length() - 2; i++){
+    if (str.substring(i, i + 3).equals("cat")){
       k_cat++;
-    }else if (str.substring(i,i+3).equals("dog")){
+    } else if (str.substring(i, i + 3).equals("dog")){
       k_dog++;
     }
   }
