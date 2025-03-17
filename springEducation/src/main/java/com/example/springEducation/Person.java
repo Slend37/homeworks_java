16dab82fe48314037d14b5da1f84406038d58d6f
Alexander Malandin <115172406+Slend37@users.noreply.github.com>
2025-03-17 10:14:54 +0300

    springEducation HW
    HW2

---
+++ springEducation/src/main/java/com/example/springEducation/Person.java
@@ -0,0 +1,48 @@
+package com.example.springEducation;
+
+import java.util.Date;
+
+public class Person {
+    String firstName;
+    String secondName;
+    Date birth;
+
+    public Person(String firstName, String secondName, Date birth) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.birth = birth;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", birth=" + birth +
+                '}';
+    }
+}
---
+++ springEducation/src/main/java/com/example/springEducation/SpringEducationApplication.java
@@ -0,0 +1,19 @@
+package com.example.springEducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringEducationApplication {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"applicationContext.xml"
+		);
+
+		Person me = context.getBean("student", Person.class);
+		SpringApplication.run(SpringEducationApplication.class, args);
+		System.out.println(me);
+	}
+}
